674019bd57d081a3ded1adaec266a0efe7fa64b3
amanda lima <lima.direito1@gmail.com>
2020-08-21 15:46:34 -0300

    adicionando main

--- javaLearning.java
+++ javaLearning.java
@@ -1,10 +1,14 @@
 public class javaLearning{
-
+    //declarando as variaveis
     private int x;
     private int y;
-    
+    //construtor
     public javaLearning(int x, int y){
         this.x = x;
         this.y = y;
     } 
+    //funções
+    public int soma(int x, int y){
+        return x+y;
+    }
 }
